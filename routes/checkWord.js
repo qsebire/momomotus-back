0b1b065dd2ec21a360f546d88581e83b08abd621
Quentin <quentin.sebire@yahoo.com>
2025-06-23 16:21:58 +0200

    Checkword with accent

--- routes/checkWord.js
+++ routes/checkWord.js
@@ -1,13 +1,16 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-const isWord = require("is-word");
-const wordsFrench = isWord("french");
+const isWord = require('is-word');
+const wordsFrench = isWord('french');
 
-router.post("/", (req, res) => {
-  const word = req.body.word;
-  const checkWord = wordsFrench.check(word.toLowerCase());
-  res.json({ result: checkWord });
+router.post('/', (req, res) => {
+    const word = req.body.word;
+    const wordWithoutAccent = word
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '');
+    const checkWord = wordsFrench.check(wordWithoutAccent.toLowerCase());
+    res.json({ result: checkWord });
 });
 
 module.exports = router;
